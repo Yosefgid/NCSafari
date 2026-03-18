47dd13c83dcbc3372830d5f9935ec2b0024a42fe
Yosefgid <jossykasshun@yahoo.com>
2026-03-18 11:23:17 +0000

    Add multiple Class and an interface

---
+++ NCSafari/Bird.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCSafari
+{
+    public abstract class Bird : Animal
+    {
+        public Bird(float weight) : base(weight)
+        {
+
+        }
+    }
+}
---
+++ NCSafari/Mammal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCSafari
+{
+    public abstract class Mammal : Animal
+    {
+        public Mammal(float weight) : base(weight)
+        {
+
+        }
+    }
+}
