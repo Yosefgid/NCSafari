3e0cdf960e3f13d465878f60fc07f2043fb3e8c7
razer <62425425+lleaob@users.noreply.github.com>
2026-03-18 17:11:25 +0000

    feat: Create a class to compare the weights of the animals in the list provided.

---
+++ NCSafari/CompareWeight.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCSafari
+{
+    public class CompareWeight: IComparer<Animal>
+    {
+        public bool Descending = false;
+        public CompareWeight(bool descending) { this.Descending = descending; }
+
+
+
+
+    }
+}
--- NCSafari/Program.cs
+++ NCSafari/Program.cs
@@ -4,10 +4,10 @@
     {
         public static void Main()
         {
-            Tiger fluffy = new Tiger(80);
-            Penguin penny = new Penguin(20.4f);
-            Goose jeff = new Goose(3);
-            Bat betty = new Bat(0.7f);
+            //Tiger fluffy = new Tiger(80);
+            //Penguin penny = new Penguin(20.4f);
+            //Goose jeff = new Goose(3);
+            //Bat betty = new Bat(0.7f);
 
             //Sky sky = new Sky();
             //sky.AddAnimalInEnvironment(betty);
@@ -22,12 +22,26 @@
 
             //water.CheckAnimalsInEnvironment();
 
-            HuntingGround hunting = new HuntingGround();
-            hunting.AddAnimalInEnvironment(betty);
-            hunting.AddAnimalInEnvironment(fluffy);
-            hunting.AddAnimalInEnvironment(penny);
+            //HuntingGround hunting = new HuntingGround();
+            //hunting.AddAnimalInEnvironment(betty);
+            //hunting.AddAnimalInEnvironment(fluffy);
+            //hunting.AddAnimalInEnvironment(penny);
 
-            hunting.CheckAnimalsInEnvironment();
+            //hunting.CheckAnimalsInEnvironment();
+
+            List<Animal> animalsToSee = new List<Animal>
+            {
+                new Tiger(70),
+                new Goose(5.2f),
+                new Bat(0.9f),
+                new Goose(4),
+                new Tiger(90),
+                new Penguin(15.8f)
+            };
+
+            animalsToSee.Sort(/*your code here!*/);
+
+            animalsToSee.ForEach(animal => Console.WriteLine($" {animal.GetType().Name} - {animal.Weight}kg"));
 
         }
     }
